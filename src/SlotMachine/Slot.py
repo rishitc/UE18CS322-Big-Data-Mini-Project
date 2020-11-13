c018259c9c7d3959ba82890d05a68d96716d0f23
Rishit Chaudhary <rishit.c@live.com>
2020-11-13 17:28:11 +0530

    Slot class for handling slots on the worker nodes

---
+++ src/SlotMachine/Slot.py
@@ -0,0 +1,5 @@
+class Slot:
+    def __init__(self, CPU_cores: int, memory: int, disk_space: int) -> None:
+        self.CPU_cores = CPU_cores
+        self.memory = memory
+        self.disk_space = disk_space
