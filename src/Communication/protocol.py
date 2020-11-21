a5eea1ba8686a727fbe017ae552f0532c46f9a1c
Rishit Chaudhary <rishit.c@live.com>
2020-11-21 21:08:03 +0530

    Finished YACS_Protocol

--- src/Communication/protocol.py
+++ src/Communication/protocol.py
@@ -1,17 +1,66 @@
 import json
+from os import stat
 
 
 class YACS_Protocol:
-    @classmethod
-    def createMessageToWorker(cls):
+    @staticmethod
+    def createMessageToWorker(job_ID, task_family, task_ID, duration,
+                              worker_ID):
         """
-        docstring
+        The final JSON string will be as follows:
+        {
+            "worker_id": <worker_id>,
+            "job_id": <job_id>,
+            "task family": <("map_tasks"|"reduce_tasks")>,
+            "task":[
+                    {
+                        "task_id": "<task_id>",
+                        "duration": <in seconds>
+                    },
+                    ]
+        }
         """
-        pass
+        msg_dict = {}
+        msg_dict["worker_id"] = worker_ID
+        msg_dict["job_id"] = job_ID
+        msg_dict["task family"] = task_family
+        msg_dict["task"] = [
+                            {
+                                "task_id": task_ID,
+                                "duration": duration
+                            }
+                            ]
+        return json.dumps(msg_dict)
 
-    @classmethod
-    def createMessageToMaster(cls):
+    @staticmethod
+    def createMessageToMaster(job_ID, task_family, task_ID, start_time,
+                              end_time, worker_ID, turnaround_time):
         """
-        docstring
+        The final JSON string will be as follows:
+        {
+            "worker_id":<worker_id>,
+            "job_id":<job_id>,
+            "task family": <("map_tasks"|"reduce_tasks")>,
+            "task":[
+                    {
+                    "task_id": "<task_id>",
+                    "start time": <arrival_time_of_task_at_Worker>,
+                    "end time": <end_time_of_task_in_Worker>,
+                    "task turnaround time": <in seconds>
+                    },
+                    ]
+        }
         """
-        pass
+        msg_dict = {}
+        msg_dict["worker_id"] = worker_ID
+        msg_dict["job_id"] = job_ID
+        msg_dict["task family"] = task_family
+        msg_dict["task"] = [
+                            {
+                                "task_id": task_ID,
+                                "start time": start_time,
+                                "end time": end_time,
+                                "task turnaround time": turnaround_time
+                            }
+                            ]
+        return json.dumps(msg_dict)
