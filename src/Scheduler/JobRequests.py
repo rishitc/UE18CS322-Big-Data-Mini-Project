7fe9f17530930b76d44971ca8e2c53225b2b8fc2
Rishit Chaudhary <rishit.c@live.com>
2020-11-21 20:03:00 +0530

    Enhanced getWaitingTask to server multiple jobs simultaneously

--- src/Scheduler/JobRequests.py
+++ src/Scheduler/JobRequests.py
@@ -11,10 +11,11 @@ class JobRequestHandler:
     Once a job has been completely allocated to one or the worker. Its entry
      is removed from this object.
     """
-    def __init__(self):
+    def __init__(self, workerUpdatesTracker):
         self.jobRequests = {}
         self.priorityOrder = []
         self.LOCK = Lock()
+        self.workerUpdatesTracker = workerUpdatesTracker
 
     def addJobRequest(self, requestSpecs):
         """Adds the request specification to the objects
@@ -33,24 +34,57 @@ class JobRequestHandler:
         }
         self.priorityOrder.append(requestSpecs["job_id"])
 
-    def getWaitingTask(self):
-        assert len(self.priorityOrder) > 0, "No job requests exist!"
+    def getWaitingTask(self, workerUpdatesTracker):
+        """getWaitingTask returns a task to be allocated on one of the workers
+        as well as task related meta-data
 
-        _JOB_ID = self.priorityOrder[0]
+        Algorithm:
+        ----------
+        1. For every job in the jobRequests list:
+            1.1 Check if the job has any pending map tasks
+                1.1.1 Return any map task of the job and associated meta-data
+            1.2 else
+                1.2.1 Check if all the job's map tasks have completed
+                    1.2.1.1 Return any reduce task of the job and associated
+                            meta-data
+                1.2.2 else
+                    1.2.2.1 Continue and hence move to the next pending job
+        2. Return None as there is no assignable task available to 
+
+        :return: Task meta-data and the task dictionary
+        :rtype: Tuple
+        """
+        # If there are no pending tasks and hence no pending jobs
+        # then return None
+        if len(self.priorityOrder) == 0:
+            return None
+
+        _JOB_ID = None
         _SELECTED_TASK = None
         _TASK_TYPE = None
+        for jobID in self.jobRequests:
+            # Check for a pending map task
+            if self.jobRequests[jobID]["map"]:
+                _SELECTED_TASK = self.jobRequests[jobID]["map"].pop(0)
+                _TASK_TYPE = "map"
+                _JOB_ID = jobID
 
-        # Check for a pending map task
-        if self.jobRequests[_JOB_ID]["map"]:
-            _SELECTED_TASK = self.jobRequests[_JOB_ID]["map"].pop(0)
-            _TASK_TYPE = "map"
+            else:
+                self.workerUpdatesTracker.LOCK.acquire()
+                _temp = self.workerUpdatesTracker.isMapComplete(jobID)
+                self.workerUpdatesTracker.LOCK.get()
+                if _temp:
+                    # Check for a pending reduce task
+                    _SELECTED_TASK = self.jobRequests[jobID]["reduce"].pop(0)
+                    _TASK_TYPE = "reduce"
+                    _JOB_ID = jobID
 
-        # Check for a pending reduce task
-        else:
-            _SELECTED_TASK = self.jobRequests[_JOB_ID]["reduce"].pop(0)
-            _TASK_TYPE = "reduce"
+        if (_SELECTED_TASK is None) and (_JOB_ID is None) and\
+           (_TASK_TYPE is None):
+            return None
 
-        # Check if this task is the last task
+        # Check if this task is the last task, if so then remove its
+        # entry from this object's state
         if (not self.jobRequests[_JOB_ID]["map"]) and \
            (not self.jobRequests[_JOB_ID]["reduce"]):
             del self.jobRequests[_JOB_ID]
