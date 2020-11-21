222e557d55ee3b610c9bfaee0465bf1174360bda
Rishit Chaudhary <rishit.c@live.com>
2020-11-21 18:51:08 +0530

    Added YACS protocol stub code

---
+++ src/Communication/protocol.py
@@ -0,0 +1,17 @@
+import json
+
+
+class YACS_Protocol:
+    @classmethod
+    def createMessageToWorker(cls):
+        """
+        docstring
+        """
+        pass
+
+    @classmethod
+    def createMessageToMaster(cls):
+        """
+        docstring
+        """
+        pass
