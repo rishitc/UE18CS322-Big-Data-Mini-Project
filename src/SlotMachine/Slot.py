9c292dabf2ad47fbf0bc6208963bae514520ba45
Rishit Chaudhary <rishit.c@live.com>
2020-11-23 16:44:32 +0530

    Removed unneeded Python file

--- src/SlotMachine/Slot.py
+++
@@ -1,5 +0,0 @@
-class Slot:
-    def __init__(self, CPU_cores: int, memory: int, disk_space: int) -> None:
-        self.CPU_cores = CPU_cores
-        self.memory = memory
-        self.disk_space = disk_space
