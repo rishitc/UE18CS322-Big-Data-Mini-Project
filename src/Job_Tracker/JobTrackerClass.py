7ae8f40bf2b09916340261e281fadf0e86e337f2
Akhil Eppa <akhil.e2k@gmail.com>
2020-11-28 18:57:23 +0530

    Updates to Job Tracker

--- src/Job_Tracker/JobTrackerClass.py
+++ src/Job_Tracker/JobTrackerClass.py
@@ -1,16 +1,52 @@
 import time
 import json
 import csv
+import os
 class JobTracker:
-    def __init__(self):
+    """
+    This Class keeps track of the various Jobs that are created.
+    The Jobs and the Tasks associated with the jobs are tracked.
+    This Class also keeps track of the mapper reducer dependency.
+    This Class creates csv log files for future analysis.
+    The log files include : 
+        *  jobs.csv -> includes job_id, start time, end time and duration
+        *  tasks.csv -> includes job_id, task_id, start time, end time and duration
+        *  workers.csv -> includes job_id, worker_id, task_id, start time and end time
+    """
+    def __init__(self, algorithm):
         self.jobs=dict()
         self.jobs_time=dict()
         self.tasks_time=dict()
+        self.workers_time=dict()
         self.map_tracker=dict()
         self.reduce_tracker=dict()
-    def add_jobs(self, request_message):
+        self.algo=algorithm
+        fields_job=['JobID','start_time','end_time','duration']
+        fields_task=['JobID','TaskID','start_time','end_time','duration']
+        fields_worker=['JobID','WorkerID','TaskID','start_time','end_time']
+        if not os.path.exists(algorithm):
+            os.mkdir(algorithm)
+        self.f_jobs=open(os.path.join(algorithm,"jobs.csv"),'w')
+        self.f_tasks=open(os.path.join(algorithm,"tasks.csv"),'w')
+        self.f_workers=open(os.path.join(algorithm, "workers.csv"),'w')
+        j_writer=csv.writer(self.f_jobs)
+        t_writer=csv.writer(self.f_tasks)
+        w_writer=csv.writer(self.f_workers)
+        j_writer.writerow(fields_job)
+        t_writer.writerow(fields_task)
+        w_writer.writerow(fields_worker)
+        self.job_writer=j_writer
+        self.task_writer=t_writer
+        self.worker_writer=w_writer
+        
+    def addJob(self, request_message):
+        """
+        Gets the Request message and initialises the dictionaries
+        to keep track of the jobs and associated tasks
+        """
         #Get job_ids from request message, update jobs and tasks
         json_string=json.loads(request_message)
+        #json_string=request_message
         #Get the job id from request string
         job_id=json_string["job_id"]
         #jobs dictionary keeps track of jobs and tasks in the job
@@ -30,6 +66,7 @@ class JobTracker:
         The start time and end time of a particular task are received in the response message
         '''
         self.tasks_time[job_id]=dict()
+        self.workers_time[job_id]=dict()
         #Initializing the dictionaries below
         for i in json_string["map_tasks"]:
             self.jobs[job_id][i["task_id"]]=None
@@ -39,12 +76,23 @@ class JobTracker:
             self.jobs[job_id][i["task_id"]]=None
             self.tasks_time[job_id][i["task_id"]]=None
             self.reduce_tracker[job_id][i["task_id"]]=0
-    def update_jobs(self, response_message):
+            
+    def updateJob(self, response_message):
+        """
+        Takes in the response message and 
+        *  Updates task end time
+        *  If all tasks composing a job are done, updates job end time
+        *  Updates task stats of a worker
+        *  Writes out the stats of job, task, worker to a csv file
+        """
         json_string=json.loads(response_message)
+        #json_string=response_message
         #Get the job id from the response message
         job_id=json_string["job_id"]
         #get the task id from the response message
         task_id=json_string["task"][0]["task_id"]
+        #Get the worker id
+        worker_id=json_string["worker_id"]
         #Get the task family
         task_fam=json_string["task family"]
         #Get task start and end time on worker
@@ -55,6 +103,10 @@ class JobTracker:
         self.jobs[job_id][task_id]=response_message
         #Stores start time and end time for a task
         self.tasks_time[job_id][task_id]=task_stats
+        self.writeTasksCSV(job_id,task_id)
+        #Store the job_id, worker_id, task_id for a particular task
+        self.workers_time[job_id][worker_id]=task_stats
+        self.writeWorkersCSV(job_id, worker_id, task_id)
         #If the task is a mapper task update map_tracker else update reduce tracker
         if task_fam=="map tasks":
             self.map_tracker[job_id][task_id]=1
@@ -72,56 +124,84 @@ class JobTracker:
                 break
         if flag==1:
             self.jobs_time[job_id][1]=time.time()
+            self.writeJobsCSV(job_id)
+            
     def isMapComplete(self,jobID):
+        """
+        performs a check whether all map tasks in a job are complete
+        This is to maintain map-reduce depenency
+        """
         status=self.map_tracker[jobID].values()
         if 0 in status:
             return 0
         else:
             return 1
+        
     def isReduceComplete(self,jobID):
+        """
+        performs a check whether all reduce tasks in a job are complete
+        This is to maintain map-reduce depenency
+        """
         status=self.reduce_tracker[jobID].values()
         if 0 in status:
             return 0
         else:
             return 1
     
-    def writeJobsCSV(self):
-        rows=[]
-        for i in self.jobs_time.keys():
-            entry=[]
-            entry.append(i)
-            start=self.jobs_time[i][0]
-            end=self.jobs_time[i][1]
-            entry.append((start))
-            entry.append((end))
-            entry.append((end-start))
-            rows.append(entry)
-        fields=['JobID','start_time','end_time','duration']
-        with open('jobs.csv','w') as f:
-            write=csv.writer(f)
-            write.writerow(fields)
-            write.writerows(rows)
+    def writeJobsCSV(self,JobID):
+        """
+        If a job is completed, writes the stats of that particular
+        job to a log file.
+        """
+        row=[]
+        row.append(JobID)
+        start=self.jobs_time[JobID][0]
+        end=self.jobs_time[JobID][1]
+        row.append(start)
+        row.append(end)
+        row.append((end-start))
+        self.job_writer.writerow(row)
+        del self.jobs_time[JobID]
+
+    def writeTasksCSV(self,JobID,TaskID):
+        """
+        If a task is completed, writes the stats of that particular
+        task to a log file.
+        """
+        row=[]
+        row.append(JobID)
+        row.append(TaskID)
+        start=self.tasks_time[JobID][TaskID][0]
+        end=self.tasks_time[JobID][TaskID][1]
+        row.append(start)
+        row.append(end)
+        row.append((end-start))
+        self.task_writer.writerow(row)
+        del self.tasks_time[JobID][TaskID]
     
-    def writeTasksCSV(self):
-        rows=[]
-        for i in self.tasks_time.keys():
-            for j in self.tasks_time[i].keys:
-                entry=[]
-                entry.append(i)
-                entry.append(j)
-                start=self.tasks_time[i][j][0]
-                end=self.tasks_time[i][j][1]
-                entry.append((start))
-                entry.append((end))
-                entry.append((end-start))
-                rows.append(entry)
-        fields=['JobID','TaskID','start_time','end_time','duration']
-        with open('tasks.csv','w') as f:
-            write=csv.writer(f)
-            write.writerow(fields)
-            write.writerows(rows)
-'''
+    def writeWorkersCSV(self,JobID,WorkerID,TaskID):
+        """
+        Writes worker stats to a log file
+        """
+        row=[]
+        row.append(JobID)
+        row.append(WorkerID)
+        row.append(TaskID)
+        start=self.workers_time[JobID][WorkerID][0]
+        end=self.workers_time[JobID][WorkerID][1]
+        row.append(start)
+        row.append(end)
+        self.worker_writer.writerow(row)
+        del self.workers_time[JobID][WorkerID]
+        
+    def closeFiles(self):
+        """
+        This closes all the open log files
+        """
+        self.f_jobs.close()
+        self.f_workers.close()
+        self.f_tasks.close()
+
 x='{"job_id":111,"map_tasks":[{"task_id":3,"duration":5},{"task_id":77,"duration":6}],"reduce_tasks":[{"task_id":4,"duration":10},{"task_id":88,"duration":12}]}'
-JB=JobTracker()
-JB.add_jobs(x)
-'''
+JB=JobTracker("rr")
+JB.addJob(x)
