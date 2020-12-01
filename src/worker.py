006416efbcad17f0a5f080cdc0668530d78fb6e6
varuntirthani <vgttir@gmail.com>
2020-12-01 16:47:27 +0530

    Adding functions for docstring and fixed linting errors
    Adding functions for docstring and fixed linting errors

--- src/worker.py
+++ src/worker.py
@@ -1,4 +1,3 @@
-import json
 import socket
 import sys
 import threading
@@ -6,8 +5,10 @@ from WorkerSim.WorkerSimulation import Worker
 
 """
 Some of the important pointers to be implemented in this code are:-
-1) Receiving the worker instance's port number and ID(in string, need to convert to integer) through CLI and binding socket to that address
-2) Listening for task launch requests from the Master's port 5001 and then simulating the execution using WorkerSimulation
+1) Receiving the worker instance's port number and ID(in string, need to
+   convert to integer) through CLI and binding socket to that address
+2) Listening for task launch requests from the Master's port 5001 and then
+   simulating the execution using WorkerSimulation
 3) Sharing updates with the master
 """
 
@@ -24,6 +25,34 @@ def getCMDLineArgs():
     """
     return int(sys.argv[1]), int(sys.argv[2])
 
+
+def createWorkerSocket(task_request_addr):
+    """ ```createWorkerSocket``` returns a socket that will be used
+    to connect worker to its designated port in order to listen for
+    task requests from the master
+
+    ```return```: Socket object
+
+    ```rtype```: socket.socket
+    """
+    socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_object.bind(task_request_addr)
+    return socket_object
+
+
+def createMasterSocket(task_complete_addr):
+    """ ```createSocket``` returns a socket that will be used
+    to connect to the master
+
+    ```return```: Socket object
+
+    ```rtype```: socket.socket
+    """
+    socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_object.connect(task_complete_addr)
+    return socket_object
+
+
 if __name__ == "__main__":
     # The CLI to the program will be python worker.py port id
     port_number, worker_id = getCMDLineArgs()
@@ -31,46 +60,48 @@ if __name__ == "__main__":
     # Creating the socket tuple for the worker where
     # it will listen to task requests from the master
     _TASK_REQUEST_ADDR = (socket.gethostname(), port_number)
-    worker_instance = Worker(worker_id)  # Instantiating instance of Worker 
-                                            #  class
-    workerPortConnSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    workerPortConnSocket.bind(_TASK_REQUEST_ADDR) # Binding the socket to the socket tuple created above
-    workerPortConnSocket.listen()  # Listens for any connections
-    masterConn, masterAddr = workerPortConnSocket.accept()  # Accepts the connection with return value being (new socket object usable to send and recv data, address bound
-                                                            #  to the socket on the other end of the connection)
-    
+    worker_instance = Worker(worker_id)  # Instance of Worker class
+    workerPortConnSocket = createWorkerSocket(_TASK_REQUEST_ADDR)
+    workerPortConnSocket.listen()
+    # workerPortConnSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # workerPortConnSocket.bind(_TASK_REQUEST_ADDR) # Binding the socket
+    # to the socket tuple created above
+    # workerPortConnSocket.listen()  # Listens for any connections
+    masterConn, masterAddr = workerPortConnSocket.accept()  # Accepts the
+    # connection with return value being (new socket object usable to send and
+    # recv data, address bound to the socket on the other end of the connection
     # ---
-    _TASK_COMPLETION_RESPONSE_ADDR = (socket.gethostname(), 5001) # Master port which takes updates on task completion from the worker
-    workerToMasterCompletionSocket = socket.socket(socket.AF_INET,
-                                                   socket.SOCK_STREAM)
-    workerToMasterCompletionSocket.connect(_TASK_COMPLETION_RESPONSE_ADDR)
+    _TASK_COMPLETION_RESPONSE_ADDR = (socket.gethostname(), 5001)  # Master
+    # port which takes updates on task completion from the worker
+    workerToMasterCompletionSocket = createMasterSocket(_TASK_COMPLETION_RESPONSE_ADDR)
     workerToMasterCompletionSocket.sendall(str(worker_id).encode())
-    # ---
-    
-    #threading.Thread(name="Received Task Request And Now Adding To Execution Pool Of Worker", target=Worker.listenForTask(worker_instance,taskRequest),args=masterConn)
-    json_receive_master: threading.Thread = threading.Thread(name="Sending Task To Exec Pool", target=worker_instance.listenForTaskRequest, args=(masterConn,))
+    # Creating all the threads
+    json_receive_master = threading.Thread(name="Sending Task To Exec Pool",
+                                           target=worker_instance.
+                                           listenForTaskRequest,
+                                           args=(masterConn,))
     json_receive_master.daemon = True
     json_receive_master.start()
 
-    json_simulate_task: threading.Thread = threading\
+    json_simulate_task = threading\
         .Thread(name="Worker Instance Simulation",
                 target=worker_instance.simulateWorker)
     json_simulate_task.daemon = True
     json_simulate_task.start()
 
-    json_reply_master: threading.Thread = threading\
+    json_reply_master = threading\
         .Thread(name=("Sending Task Completion "
                       "From Worker To Master"),
                 target=worker_instance.taskComplete,
                 args=(workerToMasterCompletionSocket,))
     json_reply_master.daemon = True
     json_reply_master.start()
-    # At the end
-    
+
+    # Joining the threads
     json_receive_master.join()
     json_simulate_task.join()
     json_reply_master.join()
 
+    # Closing the sockets
     workerPortConnSocket.close()
     workerToMasterCompletionSocket.close()
-    
