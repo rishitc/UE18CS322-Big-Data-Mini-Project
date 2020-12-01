39a3022badd2d21b482df01f5202206934309c95
Rishit Chaudhary <rishit.c@live.com>
2020-12-02 03:30:18 +0530

    Added counter for number of tasks sent by client

--- src/Copy of requests.py
+++ src/Copy of requests.py
@@ -5,16 +5,22 @@ import sys
 import random
 import numpy as np
 
+TOTAL_NO_OF_TASKS = 0
+
 def create_job_request(job_id):
+	global TOTAL_NO_OF_TASKS
+
 	number_of_map_tasks=random.randrange(1,5)
 	number_of_reduce_tasks=random.randrange(1,3)
 	job_request={"job_id":job_id,"map_tasks":[],"reduce_tasks":[]}
 	for i in range(0,number_of_map_tasks):
 		map_task={"task_id":job_id+"_M"+str(i),"duration":random.randrange(1,5)}
 		job_request["map_tasks"].append(map_task)
+		TOTAL_NO_OF_TASKS += 1
 	for i in range(0,number_of_reduce_tasks):
 		reduce_task={"task_id":job_id+"_R"+str(i),"duration":random.randrange(1,5)}
 		job_request["reduce_tasks"].append(reduce_task)
+		TOTAL_NO_OF_TASKS += 1
 	return job_request
 
 def send_request(job_request):
@@ -52,5 +58,6 @@ if __name__ == '__main__':
 		last_request_time=time.time()
 		request_number+=1
 
+	print(f"Total number of tasks sent are: {TOTAL_NO_OF_TASKS}")
 
 
