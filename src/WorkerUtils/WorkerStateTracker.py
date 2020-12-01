0ffc01756d72d13607d3aae87c90dc7e6b3d7c6b
Rishit Chaudhary <rishit.c@live.com>
2020-12-01 19:14:32 +0530

    Updated comments and reviewed code

--- src/WorkerUtils/WorkerStateTracker.py
+++ src/WorkerUtils/WorkerStateTracker.py
@@ -1,6 +1,6 @@
 import socket
 from threading import Lock
-from typing import List
+from typing import List, Optional
 
 
 class StateTracker:
@@ -8,9 +8,10 @@ class StateTracker:
         """Store the list of the worker dictionaries in a new dictionary
          indexed by the ```worker_id``` key
 
-        :param configObj: Dictionary got from loading in the json
-         data stored in the worker configuration file
-        :type configObj: dict
+        **param** ```configObj```: Dictionary got from loading in the json
+        data stored in the worker configuration file
+
+        **type** ```configObj```: dict
         """
         self.workerState = {}
         self.workerIDs: List[int] = []
@@ -33,16 +34,21 @@ class StateTracker:
         """Check if the worker whose ```worker_id``` key is equal to
          workerID.
 
-        :param workerID: ```worker_id``` value of the worker node we are
-         checking for free slots
-        :type workerID: int
-        :param demand: Specifies the number of free slots we are looking
-         for in the worker node with ```worker_id``` equal to workerID,
-         defaults to 1
-        :type demand: int, optional
-        :return: Returns True if the number of free slots given by ```demand```
-         are found, else it returns false
-        :rtype: bool
+        **param** ```workerID```: ```worker_id``` value of the worker node
+        we are checking for free slots
+
+        **type** ```workerID```: int
+
+        **param** ```demand```: Specifies the number of free slots we are
+        looking for in the worker node with ```worker_id``` equal to workerID,
+        defaults to 1
+
+        **type** ```demand```: int, optional
+
+        **return**: Returns True if the number of free slots given by
+        ```demand``` are found, else it returns false
+
+        **rtype**: bool
         """
         return True if self.workerState[workerID]["free slots"] >= demand \
             else False
@@ -57,12 +63,15 @@ class StateTracker:
         """Allocates the task to the worker and decrements the number of
          free slots in that worker
 
-        :param workerID: Specifies the worker to which we are allocating the
-         task to
-        :type workerID: int
-        :param task_count: Specifies the number of tasks being allocated the
-         worker, defaults to 1
-        :type task_count: int, optional
+        **param** ```workerID```: Specifies the worker to which we are
+        allocating the task to
+
+        **type** ```workerID```: int
+
+        **param** ```task_count```: Specifies the number of tasks being
+        allocated the worker, defaults to 1
+
+        **type** ```task_count```: int, optional
         """
         assert self.isWorkerFree(workerID, task_count) is True,\
             "Over allocating tasks to worker!"
@@ -70,20 +79,24 @@ class StateTracker:
 
     def freeSlot(self, workerID: int, task_count: int = 1) -> None:
         """Updates the state of the worker to indicate task completion
-         by incrementing the number of free slots on that worker
+        by incrementing the number of free slots on that worker
 
-        :param workerID: Specifies the worker which has completed its task
-        :type workerID: int
-        :param task_count: Specifies the number of tasks compelted by the
-         worker, defaults to 1
-        :type task_count: int, optional
+        **param** ```workerID```: Specifies the worker which has completed its
+        task
+
+        **type** ```workerID```: int
+
+        **param** ```task_count```: Specifies the number of tasks compelted by
+        the worker, defaults to 1
+
+        **type** ```task_count```: int, optional
         """
         assert self.workerState[workerID]["free slots"] != \
             self.workerState[workerID]["slots"],\
             "There are no slots to free up!"
         self.workerState[workerID]["free slots"] += task_count
 
-    def getLeastLoadedWorkerID(self):
+    def getLeastLoadedWorkerID(self) -> Optional[int]:
         # Variables used to track the least loaded worker
         _least_loaded_workerID = None
         _least_loaded_workerFreeSlots = 0
