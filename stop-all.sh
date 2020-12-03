2aa957b37b3fae5f287aa0afa25aa94feae8048b
Rishit Chaudhary <rishit.c@live.com>
2020-12-03 19:37:09 +0530

    Updated program termination code

---
+++ stop-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4000\e[0m"
+sudo fuser -k 4000/tcp
+
+echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4001\e[0m"
+sudo fuser -k 4001/tcp
+
+echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4002\e[0m"
+sudo fuser -k 4002/tcp
+
+echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m5000\e[0m"
+sudo fuser -k 5000/tcp
+
+echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m5001\e[0m"
+sudo fuser -k 5001/tcp
