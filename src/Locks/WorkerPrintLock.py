c0e74e0b854d4f6269b50ddd9f17a40e13464a5a
Rishit Chaudhary <rishit.c@live.com>
2020-12-04 01:19:58 +0530

    Updated documentation and docstring

--- src/Locks/WorkerPrintLock.py
+++ src/Locks/WorkerPrintLock.py
@@ -5,7 +5,7 @@ class WorkerPrintLock:
     """ This class is used to create the *common object* across
     the files for the **worker code** to share the ```PRINT_LOCK```, so that
     there are **no race condition**, i.e. in this case *garbled text* on the
-    CLI output, due to multiple threads trying to print onto the CLI all at
+    CLI output, due to multiple threads trying to print onto the CLI, all at
     once.
     """
     PRINT_LOCK = threading.Lock()
