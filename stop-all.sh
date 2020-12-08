22b64c19b0d2c89dd3c4aa0e41e7c9780e3c2ad1
Rishit Chaudhary <rishit.c@live.com>
2020-12-08 16:17:33 +0530

    Improved stop-all output

--- stop-all.sh
+++ stop-all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4000\e[0m"
-sudo fuser -k 4000/tcp
+sudo fuser -k 4000/tcp 2>/dev/null
 
 echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4001\e[0m"
-sudo fuser -k 4001/tcp
+sudo fuser -k 4001/tcp 2>/dev/null
 
 echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m4002\e[0m"
-sudo fuser -k 4002/tcp
+sudo fuser -k 4002/tcp 2>/dev/null
 
 echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m5000\e[0m"
-sudo fuser -k 5000/tcp
+sudo fuser -k 5000/tcp 2>/dev/null
 
 echo -e "\e[1m\e[36mINFO:\e[0m Stopping process/thread running at port \e[1m5001\e[0m"
-sudo fuser -k 5001/tcp
+sudo fuser -k 5001/tcp 2>/dev/null
